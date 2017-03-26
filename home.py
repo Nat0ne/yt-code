9c3cb9a346a37c287e9149338b13e6e48b5334da
Nat0ne <ampeleuterio@gmail.com>
2017-03-26 13:45:48 +0000

    Distinction between series and movies

--- home.py
+++ home.py
@@ -7,7 +7,7 @@ from upload_video import UploadVideo
 VIDEOS_FOLDER = "/home/pi/Youtube/Outbox/"
 
 
-videos_list = listdir(VIDEOS_FOLDER)[::-1]
+videos_list = sorted(listdir(VIDEOS_FOLDER))
 
 video = {'file': '', 'title': '', 'description': '', 'keywords': '', 'category': 22}
 
@@ -16,21 +16,28 @@ for i in range(0, len(videos_list)):
   video['description'] = videos_list[i]
   video['file'] = VIDEOS_FOLDER + video['description']
 
-  # Find season
-  video_season = re.search('(?<=s|S)\d{1,2}', video['description']).group(0)
-  video_season = int(video_season) + 100
-  video_season = str(video_season)[1:3]
-
-  # Find episode
-  video_episode = re.search('(?<=e|E|p|P)\d{1,2}', video['description']).group(0)
-  video_episode = int(video_episode) + 100
-  video_episode = str(video_episode)[1:3]
-
   # Change dots to spaces
   video['title'] = video['description'][:-4].replace('.',' ') + video['description'][-4:]
-  video['title'] = re.search('[^(s)|(S)\d{1,2}]*', video['title']).group(0)
-  video['title'] = video['title'] + "S" + video_season + "E" + video_episode
+
+  # Find season
+  video_season = re.search('(?<=s|S)\d{1,2}', video['description'])
+
+  # Find episode
+  video_episode = re.search('(?<=e|E|p|P)\d{1,2}', video['description'])
 
 
+  if video_season and video_episode:   # It is a series
+    video_season = int(video_season.group(0)) + 100
+    video_season = str(video_season)[1:3]
+
+    video_episode = int(video_episode.group(0)) + 100
+    video_episode = str(video_episode)[1:3]
+
+    video['title'] = re.search('([\w\s,-]+)\s(s|S)\d{1,2}', video['title']).group(1)
+    video['title'] = video['title'] + " S" + video_season + "E" + video_episode
+
+  else: # it is a movie
+    print "It is a movie"
+  
   upload_video1 = UploadVideo()
   upload_video1.upload_video_to_youtube(video)
